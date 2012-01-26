9e9ef08a4b8cda03801023c330213db8dabf2523
Jade Dominguez <plusjade@gmail.com>
2012-01-25 19:41:54 -0800

    refactor theme explorer javascript

--- assets/theme-explorer/init.js
+++ assets/theme-explorer/init.js
@@ -1,31 +1,49 @@
 $(function(){
 	
-  var $themeExplorer = $("#theme_explorer");
-  var themes = ["twitter", "the-minimum", "tom", "mark-reid"];
-  var cache = "";
-  
-  themes.forEach(function(theme){
-    cache += '<li><a href="/themes/'+ theme +'" class="'+ theme +'">'+ theme +'</a></li>';
-  })
-  $themeExplorer.find("ul.te_themes").html(cache);
+	var TE = {
+		$container : $("#theme_explorer"),
+		currentTheme : location.pathname.split("/")[2],
+		$toggler : $("#theme_explorer_hide"), 
 
-  $("#theme_explorer_hide").click(function(e){
-    $themeExplorer.animate({
-			bottom: parseInt($themeExplorer.css('bottom'),10) == 0 ? -$themeExplorer.outerHeight() : 0
-		});
-		$(this).text( ($(this).text() === "HIDE") ? "SHOW" : "HIDE" );
-
-    e.preventDefault();
-    return false;
-  })
-
-	// Set current theme ;
-	var currentTheme = location.pathname.split("/")[2];
-	
-	if(typeof currentTheme !== "undefined"){
-		$themeExplorer.find(".current_theme").find("span").text(currentTheme);
-		$themeExplorer.find("ul.te_themes").find("a."+ currentTheme).addClass("active");
+	// initialize theme explorer
+		init : function(){
+			TE.$toggler.click(function(e){
+		    TE.toggle();
+				e.preventDefault();
+		    return false;
+		  })
+			
+			TE.loadThemes();
+			TE.setActiveTheme();
+		},
+		
+	// populate themes into theme explorer
+		loadThemes : function(){
+			var themes = ["twitter", "the-minimum", "tom", "mark-reid"];
+		  var cache = "";
+		  themes.forEach(function(theme){
+		    cache += '<li><a href="/themes/'+ theme +'" class="'+ theme +'">'+ theme +'</a></li>';
+		  })
+		  TE.$container.find("ul.te_themes").html(cache);
+		},
+		
+	// show/hide theme explorer
+		toggle : function(){
+			TE.$container.animate({
+				bottom: parseInt(TE.$container.css('bottom'),10) == 0 ? -TE.$container.outerHeight() : 0
+			});
+			TE.$toggler.text( (TE.$toggler.text() === "HIDE") ? "SHOW" : "HIDE" );
+		},
+		
+	// Set current active theme
+		setActiveTheme : function(){
+			if(typeof TE.currentTheme !== "undefined"){
+				TE.$container.find(".current_theme").find("span").text(TE.currentTheme);
+				TE.$container.find("ul.te_themes").find("a."+ TE.currentTheme).addClass("active");
+			}
+		}
+		
 	}
-	
-	
+
+	TE.init();
 })
